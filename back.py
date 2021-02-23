466bd607908a93d902135f60ac2c36c063c0b087
oppsec <daniel.sdm.github@gmail.com>
2021-02-23 19:03:09 -0300

    code: revamped the main file

--- back.py
+++ back.py
@@ -2,61 +2,35 @@
 # -*- coding: utf-8 -*-
 # Author: Gr3n0xX
 # Python backdoor generator 
+
 try:
     import sys
     import os
     import time
+
     from payloads.pythonpayload import pyload
-    from src.loanding import load
-    from colorama import Fore, Back, Style
-    
-except ModuleNotFoundError:
-    print(Fore.RED + '[!] I require colorama, install it')
-    sys.exit()
+   
+    from src.message import success, error
+    from src.clear import clear_terminal
+    from src.ascii import get_ascii
+
 except KeyboardInterrupt:
-    print(Fore.RED + '[!] Exiting.')
-    sys.exit()
-if sys.version_info[0] < 3:
-    print("\033[1m\033[93m[!] Please run the tool using Python 3")
+    error("Exiting...")
     sys.exit()
 
+if sys.version_info[0] < 3:
+    error("Run this tool with Python 3 please.")
+    sys.exit()
 
     
 def banner():
-    os.system("clear")
-    print (Fore.BLUE + """
+    clear_terminal()
+    get_ascii()
 
-           _________
-         /'        /|
-        /         / |_
-       /         /  //|
-      /_________/  ////|
-     |   _ _    | 8o////|
-     | /'// )_  |   8///|
-     |/ // // ) |   8o///|
-     / // // //,|  /  8//|
-    / // // /// | /   8//|
-   / // // ///__|/    8//|
-  /.(_)// /// |       8///|
- (_)' `(_)//| |       8////|___________
-(_) /_\ (_)'| |        8///////////////
-(_) \_/ (_)'|_|         8/////////////
- (_)._.(_) d' Hb         8oooooooopb'
-   `(_)'  d'  H`b       -------------------
-         d'   `b`b      |    BACKDOOR     |
-        d'     H `b     |    Generator    |
-       d'      `b `b    |     AUTHOR:     |
-      d'           `b   |    Gr3n0xX      |
-     d'             `b  |                 |
-                        -------------------
+def main():
+    clear_terminal()
+    get_ascii()
+    pyload()
 
-Visit https://github.com/grenoxx For More Tools:""")
-
-
-    
-    
-banner()
-pyload()
-load()
-os.system("clear")
-print ("[*] Finish!!!")
+if __name__ == "__main__":
+    main()
